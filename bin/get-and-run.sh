9e284d29e23600454f4667cd9d8c959c3b2008db
bkarstens <barrettkarstens@gmail.com>
2016-07-05 12:50:10 -0500

    can now pip install project, made os dependent code more flexible

--- bin/get-and-run.sh
+++ bin/get-and-run.sh
@@ -1,7 +1,17 @@
-sudo pip apt-get install python-dev
-sudo pip install -r /opt/Sector67RaspberryPiAccessControl/requirements.txt
-rm -rf /opt/Sector67RaspberryPiAccessControl
-cd /opt
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+PYTHON=$(which python)
+var1="Linux2"
+var2=$(uname -a)
+if [[ "$var2" =~ "$var1" ]]; then
+    echo "OS is Linux2, running apt-get"
+    sudo apt-get install python-dev
+	sudo apt-get install vlc
+else
+    echo "OS is not Linux2, python-dev and vlc might need to be installed manually."
+fi
+cd 
+rm -rf $DIR/../../Sector67RaspberryPiAccessControl
 git clone https://github.com/bvesperman/Sector67RaspberryPiAccessControl.git
-cd /opt/Sector67RaspberryPiAccessControl/space_machines
-python main.py rpi-machine.conf
+sudo python -m pip install Sector67RaspberryPiAccessControl
+cd Sector67RaspberryPiAccessControl/space_machines/
+$PYTHON main.py rpi-machine.conf
---
+++ setup.py
@@ -0,0 +1,34 @@
+from sys import platform
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+exec(open('space_machines/version.py').read())
+
+setup(
+    name='space_machines',
+    version=__version__,
+    author='Brian Vesperman',
+    author_email='bvesperman@gmail.com',
+    url='https://github.com/bvesperman/Sector67RaspberryPiAccessControl',
+    packages=['space_machines'],
+    license='MIT',
+    description='Controlling or interfacing with simple machines from a Raspberry Pi.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'RPi.GPIO' if platform=='linux2' else '',
+        'rpi_ws281x' if platform=='linux2' else '',
+        'expiringdict',
+        'suds',
+        'python_vlc',
+        'gTTS'
+    ],
+    #test_suite='nose.collector',
+    #tests_require=['nose'],
+    classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Development Status :: 2 - Pre-Alpha',
+          'Programming Language :: Python :: 2.7',
+    ],
+)
