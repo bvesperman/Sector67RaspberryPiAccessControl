1c71a5815717d06013860f1489e8d32e51f87aa8
bkarstens <barrettkarstens@gmail.com>
2016-07-05 13:26:37 -0500

    os dependencies more flexible

--- bin/get-and-run.sh
+++ bin/get-and-run.sh
@@ -1,17 +1,25 @@
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-PYTHON=$(which python)
-var1="Linux2"
-var2=$(uname -a)
-if [[ "$var2" =~ "$var1" ]]; then
+cd ../..
+DIR=$(pwd)
+os="Linux2"
+kernalinfo=$(uname -a)
+if [[ "$kernalinfo" =~ "$os" ]]; then
+	isRPi=True
     echo "OS is Linux2, running apt-get"
     sudo apt-get install python-dev
 	sudo apt-get install vlc
 else
-    echo "OS is not Linux2, python-dev and vlc might need to be installed manually."
+	isRPi=False
+    echo "OS is not Linux2, vlc might need to be installed manually."
 fi
-cd 
-rm -rf $DIR/../../Sector67RaspberryPiAccessControl
+rm -rf $DIR/Sector67RaspberryPiAccessControl
+
 git clone https://github.com/bvesperman/Sector67RaspberryPiAccessControl.git
+sudo python -m pip uninstall Sector67RaspberryPiAccessControl
 sudo python -m pip install Sector67RaspberryPiAccessControl
-cd Sector67RaspberryPiAccessControl/space_machines/
-$PYTHON main.py rpi-machine.conf
+
+cd $DIR/Sector67RaspberryPiAccessControl/space_machines/
+if isRPi; then
+	python main.py rpi-machine.conf
+else
+	python main.py machine.conf
+fi
--- space_machines/rpioutput.py
+++ space_machines/rpioutput.py
@@ -1,49 +1,53 @@
-import logging
-import time
-import Queue
-import threading
+import sys
+if sys.platform=='linux2':
 
-import RPi.GPIO as GPIO
+  import logging
+  import time
+  import Queue
+  import threading
+  
+  import RPi.GPIO as GPIO
 
-from pystates import StateMachine
+  from pystates import StateMachine
 
-class RpiGpioOutput(StateMachine):
+  class RpiGpioOutput(StateMachine):
 
-  def ON(self):
-    self.logger.debug(self.name + " output turned on ")
-    GPIO.output(self.gpio_pin, 1)
-    while True:
-      ev = yield
-      if ev['event'] == self.off_message:
-        self.transition(self.OFF)
+    def ON(self):
+      self.logger.debug(self.name + " output turned on ")
+      GPIO.output(self.gpio_pin, 1)
+      while True:
+        ev = yield
+        if ev['event'] == self.off_message:
+          self.transition(self.OFF)
 
-  def OFF(self):
-    self.logger.debug(self.name + " output turned off")
-    GPIO.output(self.gpio_pin, 0)
-    while True:
-      ev = yield
-      if ev['event'] == self.on_message:
-        self.transition(self.ON)
+    def OFF(self):
+      self.logger.debug(self.name + " output turned off")
+      GPIO.output(self.gpio_pin, 0)
+      while True:
+        ev = yield
+        if ev['event'] == self.on_message:
+          self.transition(self.ON)
 
-  def setup(self, out_queue, name, gpio_pin, on_message, off_message, initial_state):
-    self.logger = logging.getLogger("RpiGpioOutput")
-    self.out_queue = out_queue
-    self.name = name
-    self.gpio_pin=int(gpio_pin)
-    self.on_message = on_message
-    self.off_message = off_message
-    self.initial_state = initial_state
+    def setup(self, out_queue, name, gpio_pin, on_message, off_message, initial_state):
+      self.logger = logging.getLogger("RpiGpioOutput")
+      self.out_queue = out_queue
+      self.name = name
+      self.gpio_pin=int(gpio_pin)
+      self.on_message = on_message
+      self.off_message = off_message
+      self.initial_state = initial_state
 
-  """ Perform initialization here
-  """
-  def start(self):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(self.gpio_pin, GPIO.OUT)
-    if (self.initial_state == "ON"):
-      self.logger.debug("initial state: " + self.on_message)
-      super(RpiGpioOutput, self).start(self.ON)
-    else:
-      self.logger.debug("initial state: " + self.off_message)
-      super(RpiGpioOutput, self).start(self.OFF)
-    
-
+    """ Perform initialization here
+    """
+    def start(self):
+      GPIO.setmode(GPIO.BCM)
+      GPIO.setup(self.gpio_pin, GPIO.OUT)
+      if (self.initial_state == "ON"):
+        self.logger.debug("initial state: " + self.on_message)
+        super(RpiGpioOutput, self).start(self.ON)
+      else:
+        self.logger.debug("initial state: " + self.off_message)
+        super(RpiGpioOutput, self).start(self.OFF)
+else:
+  from guiswitch import *
+  RpiGpioSwitch = GuiSwitch
