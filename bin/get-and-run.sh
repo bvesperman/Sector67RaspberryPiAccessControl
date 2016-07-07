186b67c3fdec6beaea4e57a5966eb2e86a6a5d5b
bkarstens <barrettkarstens@gmail.com>
2016-07-07 18:03:01 -0500

    trying different method to get service to work

--- bin/get-and-run.sh
+++ bin/get-and-run.sh
@@ -5,6 +5,7 @@ cd ..
 sudo git clone https://github.com/bvesperman/Sector67RaspberryPiAccessControl.git
 cd Sector67RaspberryPiAccessControl
 DIR=$(pwd)
+START=$DIR/bin/start.sh
 PYTHON=$(which python)
 os='Linux raspberrypi'
 MAIN=$DIR/space_machines/main.py
@@ -16,11 +17,13 @@ if [[ "$kernalinfo" =~ "$os" ]]; then
     sudo apt-get install python-dev
 	sudo apt-get install vlc
 	sudo sed -i.backup 's|.*BLANK_TIME=.*|BLANK_TIME=0|; s|.*BLANK_DPMS=.*|BLANK_DPMS=off|; s|.*POWERDOWN_TIME=.*|POWERDOWN_TIME=0|' /etc/kbd/config #disable screensaver/ screen blanking
-	sudo sed -i.backup "s|#PYTHONPATH#|$PYTHON|; s|#FILEPATH#|$MAIN|; s|#CONFPATH#|$CONF|" $DIR/bin/space_machines.service # enters the file path into the service file
+	sudo sed -i.backup "s|#PYTHONPATH#|$PYTHON|; s|#FILEPATH#|$MAIN|; s|#CONFPATH#|$CONF|" $START # enters the file path into the start up file
+	sudo sed -i.backup "s|#STARTPATH#|$START|" $DIR/bin/space_machines.service # enters the start up file path into the service file
 	sudo cp $DIR/bin/space_machines.service /lib/systemd/system/space_machines.service #makes script run on boot
 	sudo chmod 644 /lib/systemd/system/space_machines.service
 	sudo systemctl daemon-reload
 	sudo systemctl enable space_machines.service
+	#sudo systemctl enable space_machines.service
 	sudo systemctl disable serial-getty@ttyAMA0.service #disable serial TTY
 else
     echo "OS is not $os, vlc might need to be installed manually."
@@ -33,4 +36,4 @@ cd $DIR
 sudo $PYTHON -m pip uninstall $DIR
 sudo $PYTHON -m pip install $DIR
 
-sudo $PYTHON $MAIN $CONF
+sudo $START
