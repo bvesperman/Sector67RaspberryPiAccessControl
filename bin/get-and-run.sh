dd50ad9c9c60199ad0dfd2cd533202b6a7b75bf5
bkarstens <barrettkarstens@gmail.com>
2016-07-07 17:48:25 -0500

    asdf

--- bin/get-and-run.sh
+++ bin/get-and-run.sh
@@ -5,6 +5,7 @@ cd ..
 sudo git clone https://github.com/bvesperman/Sector67RaspberryPiAccessControl.git
 cd Sector67RaspberryPiAccessControl
 DIR=$(pwd)
+PYTHON=$(which python)
 os='Linux raspberrypi'
 MAIN=$DIR/space_machines/main.py
 kernalinfo=$(uname -a)
@@ -15,7 +16,7 @@ if [[ "$kernalinfo" =~ "$os" ]]; then
     sudo apt-get install python-dev
 	sudo apt-get install vlc
 	sudo sed -i.backup 's|.*BLANK_TIME=.*|BLANK_TIME=0|; s|.*BLANK_DPMS=.*|BLANK_DPMS=off|; s|.*POWERDOWN_TIME=.*|POWERDOWN_TIME=0|' /etc/kbd/config #disable screensaver/ screen blanking
-	sudo sed -i.backup "s|#FILEPATH#|$MAIN|; s|#CONFPATH#|$CONF|" $DIR/bin/space_machines.service # enters the file path into the service file
+	sudo sed -i.backup "s|#PYTHONPATH#|$PYTHON|; s|#FILEPATH#|$MAIN|; s|#CONFPATH#|$CONF|" $DIR/bin/space_machines.service # enters the file path into the service file
 	sudo cp $DIR/bin/space_machines.service /lib/systemd/system/space_machines.service #makes script run on boot
 	sudo chmod 644 /lib/systemd/system/space_machines.service
 	sudo systemctl daemon-reload
@@ -29,7 +30,7 @@ fi
 #cd $DIR
 #git checkout Better-dependencies
 cd $DIR
-sudo python -m pip uninstall $DIR
-sudo python -m pip install $DIR
+sudo $PYTHON -m pip uninstall $DIR
+sudo $PYTHON -m pip install $DIR
 
-sudo python $MAIN $CONF
+sudo $PYTHON $MAIN $CONF
