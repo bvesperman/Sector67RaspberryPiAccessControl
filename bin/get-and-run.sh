49f3d984db6b7aa7a126e4ac9ae72a5ddd2d8947
bkarstens <barrettkarstens@gmail.com>
2016-07-07 09:54:17 -0500

    Fixed bug
    the Sector67Ra... directory gets deleted and replaced so when
    attempting to access the directory an error occurs.

--- bin/get-and-run.sh
+++ bin/get-and-run.sh
@@ -1,6 +1,6 @@
-cd ..
+cd ../..
 DIR=$(pwd)
-MAIN=$DIR/space_machines/main.py
+MAIN=$DIR/Sector67RaspberryPiAccessControl/space_machines/main.py
 os="Linux2"
 kernalinfo=$(uname -a)
 if [[ "$kernalinfo" =~ "$os" ]]; then
@@ -9,8 +9,8 @@ if [[ "$kernalinfo" =~ "$os" ]]; then
     sudo apt-get install python-dev
 	sudo apt-get install vlc
 	sed -i.backup 's|.*BLANK_TIME=.*|BLANK_TIME=0|; s|.*BLANK_DPMS=.*|BLANK_DPMS=off|; s|.*POWERDOWN_TIME=.*|POWERDOWN_TIME=0|' /etc/kbd/config #disable screensaver/ screen blanking
-	sed -i.backup 's|#FILEPATH#|'$MAIN'|' $DIR/bin/space_machines.service # enters the file path into the service file
-	sudo cp $DIR/bin/space_machines.service /lib/systemd/system/space_machines.service #makes script run on boot
+	sed -i.backup 's|#FILEPATH#|'$MAIN'|' $DIR/Sector67RaspberryPiAccessControl/bin/space_machines.service # enters the file path into the service file
+	sudo cp $DIR/Sector67RaspberryPiAccessControl/bin/space_machines.service /lib/systemd/system/space_machines.service #makes script run on boot
 	sudo chmod 644 /lib/systemd/system/space_machines.service
 	sudo systemctl daemon-reload
 	sudo systemctl enable space_machines.service
@@ -19,14 +19,14 @@ else
 	isRPi=False
     echo "OS is not Linux2, vlc might need to be installed manually."
 fi
-rm -rf $DIR
+rm -rf $DIR/Sector67RaspberryPiAccessControl
 
 git clone https://github.com/bvesperman/Sector67RaspberryPiAccessControl.git
-#cd $DIR
+#cd $DIR/Sector67RaspberryPiAccessControl
 #git checkout Better-dependencies
-cd $DIR
-sudo python -m pip uninstall $DIR
-sudo python -m pip install $DIR
+cd $DIR/Sector67RaspberryPiAccessControl
+sudo python -m pip uninstall $DIR/Sector67RaspberryPiAccessControl
+sudo python -m pip install $DIR/Sector67RaspberryPiAccessControl
 
 if $isRPi; then
 	python $MAIN rpi-machine.conf
