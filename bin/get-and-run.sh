763c9628c91b8b759cba7f404ee9394621d9ba43
bkarstens <barrettkarstens@gmail.com>
2016-07-07 11:03:55 -0500

    Actually problem was running as sh not bash
    plus T/F must be lowercase

--- bin/get-and-run.sh
+++ bin/get-and-run.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-cd ../..
+cd ..
 DIR=$(pwd)
-MAIN=$DIR/Sector67RaspberryPiAccessControl/space_machines/main.py
 os="Linux raspberrypi"
+MAIN=$DIR/space_machines/main.py
 kernalinfo=$(uname -a)
 if [[ "$kernalinfo" =~ "$os" ]]; then
-	isRPi='True'
     echo "OS is $os, running apt-get"
+	isRPi=true
     sudo apt-get install python-dev
 	sudo apt-get install vlc
 	sed -i.backup 's|.*BLANK_TIME=.*|BLANK_TIME=0|; s|.*BLANK_DPMS=.*|BLANK_DPMS=off|; s|.*POWERDOWN_TIME=.*|POWERDOWN_TIME=0|' /etc/kbd/config #disable screensaver/ screen blanking
-	sed -i.backup 's|#FILEPATH#|'$MAIN'|' $DIR/Sector67RaspberryPiAccessControl/bin/space_machines.service # enters the file path into the service file
-	sudo cp $DIR/Sector67RaspberryPiAccessControl/bin/space_machines.service /lib/systemd/system/space_machines.service #makes script run on boot
+	sed -i.backup 's|#FILEPATH#|'$MAIN'|' $DIR/bin/space_machines.service # enters the file path into the service file
+	sudo cp $DIR/bin/space_machines.service /lib/systemd/system/space_machines.service #makes script run on boot
 	sudo chmod 644 /lib/systemd/system/space_machines.service
 	sudo systemctl daemon-reload
 	sudo systemctl enable space_machines.service
 	sudo systemctl disable serial-getty@ttyAMA0.service #disable serial TTY
 else
-	isRPi='False'
     echo "OS is not $os, vlc might need to be installed manually."
+	isRPi=false
 fi
-rm -rf $DIR/Sector67RaspberryPiAccessControl
+rm -rf $DIR
 
 git clone https://github.com/bvesperman/Sector67RaspberryPiAccessControl.git
-#cd $DIR/Sector67RaspberryPiAccessControl
+#cd $DIR
 #git checkout Better-dependencies
-cd $DIR/Sector67RaspberryPiAccessControl
-sudo python -m pip uninstall $DIR/Sector67RaspberryPiAccessControl
-sudo python -m pip install $DIR/Sector67RaspberryPiAccessControl
+cd $DIR
+sudo python -m pip uninstall $DIR
+sudo python -m pip install $DIR
 
-if [ $isRPi == 'True' ]; then
+if $isRPi; then
 	python $MAIN rpi-machine.conf
 else
 	python $MAIN machine.conf
