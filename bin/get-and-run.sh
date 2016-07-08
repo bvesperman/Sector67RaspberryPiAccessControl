2708942b5ad01cc72b0d49c09094686c17dd643e
Barrett Karstens <barrettkarstens@gmail.com>
2016-07-07 19:16:21 -0500

    consistent tabs and efficiency

--- bin/get-and-run.sh
+++ bin/get-and-run.sh
@@ -13,11 +13,10 @@ os='Linux raspberrypi'
 MAIN=$DIR/space_machines/main.py
 kernalinfo=$(uname -a)
 if [[ "$kernalinfo" =~ "$os" ]]; then
-    echo "OS is $os, running apt-get"
+	echo "OS is $os, running apt-get"
 	isRPi=true
 	CONF=$DIR/space_machines/rpi-machine.conf
-    sudo apt-get install python-dev
-	sudo apt-get install vlc
+	sudo apt-get install python-dev vlc
 	sudo sed -i.backup 's|.*BLANK_TIME=.*|BLANK_TIME=0|; s|.*BLANK_DPMS=.*|BLANK_DPMS=off|; s|.*POWERDOWN_TIME=.*|POWERDOWN_TIME=0|' /etc/kbd/config #disable screensaver/ screen blanking
 	sudo sed -i.backup "s|#PYTHONPATH#|$PYTHON|; s|#FILEPATH#|$MAIN|; s|#CONFPATH#|$CONF|" $START # enters the file path into the start up file
 	sudo sed -i.backup "s|#STARTPATH#|$START|" $DIR/bin/space_machines.service # enters the start up file path into the service file
@@ -28,7 +27,7 @@ if [[ "$kernalinfo" =~ "$os" ]]; then
 	#sudo systemctl enable space_machines.service
 	sudo systemctl disable serial-getty@ttyAMA0.service #disable serial TTY
 else
-    echo "OS is not $os, vlc might need to be installed manually."
+	echo "OS is not $os, vlc might need to be installed manually."
 	isRPi=false
 	CONF=$DIR/space_machines/machine.conf
 fi
