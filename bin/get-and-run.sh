33e5b8fc924e2ab237b8c6c8293e89a27d30707a
Barrett Karstens <barrettkarstens@gmail.com>
2016-07-07 19:42:34 -0500

    second half of previous commit

--- bin/get-and-run.sh
+++ bin/get-and-run.sh
@@ -19,7 +19,7 @@ if [[ "$kernalinfo" =~ "$os" ]]; then
 	CONF=$DIR/space_machines/rpi-machine.conf
 	sudo apt-get install python-dev vlc
 	sudo sed -i.backup 's|.*BLANK_TIME=.*|BLANK_TIME=0|; s|.*BLANK_DPMS=.*|BLANK_DPMS=off|; s|.*POWERDOWN_TIME=.*|POWERDOWN_TIME=0|' /etc/kbd/config #disable screensaver/ screen blanking
-	sudo sed -i.backup "s|#PYTHONPATH#|$PYTHON|; s|#FILEPATH#|$MAIN|; s|#CONFPATH#|$CONF|" $START # enters the file path into the start up file
+	sudo sed -i.backup "s|#PYTHONPATH#|$PYTHON|; s|#FILEPATH#|$MAIN|; s|#CONFPATH#|$CONF|; s|#LOGPATH#|$LOG|" $START # enters the file path into the start up file
 	sudo sed -i.backup "s|#STARTPATH#|$START|" $DIR/bin/space_machines.service # enters the start up file path into the service file
 	sudo cp $DIR/bin/space_machines.service /lib/systemd/system/space_machines.service #makes script run on boot
 	sudo chmod 644 /lib/systemd/system/space_machines.service
