6823e964193c4fe7ed95026f7122ee15ca9ad35a
bkarstens <barrettkarstens@gmail.com>
2016-07-08 20:21:11 -0500

    undo pid work for testing

--- bin/get-and-run.sh
+++ bin/get-and-run.sh
@@ -8,7 +8,6 @@ git checkout pseudo-master
 DIR=$(pwd)
 START=$DIR/bin/start.sh
 LOG=$DIR/space_machines/logging.conf
-PID=/var/run/space_machines.pid
 sudo chmod 755 $START
 PYTHON=$(which python)
 os='Linux raspberrypi'
@@ -20,8 +19,8 @@ if [[ "$kernalinfo" =~ "$os" ]]; then
 	CONF=$DIR/space_machines/rpi-machine.conf
 	sudo apt-get install python-dev vlc
 	sudo sed -i.backup 's|.*BLANK_TIME=.*|BLANK_TIME=0|; s|.*BLANK_DPMS=.*|BLANK_DPMS=off|; s|.*POWERDOWN_TIME=.*|POWERDOWN_TIME=0|' /etc/kbd/config #disable screensaver/ screen blanking
-	sudo sed -i.backup "s|#PYTHONPATH#|$PYTHON|; s|#FILEPATH#|$MAIN|; s|#CONFPATH#|$CONF|; s|#LOGPATH#|$LOG|; s|#PIDPATH#|$PID|" $START # enters the file path into the start up file
-	sudo sed -i.backup "s|#STARTPATH#|$START|; s|#PIDPATH#|$PID|" $DIR/bin/space_machines.service # enters the start up file path into the service file
+	sudo sed -i.backup "s|#PYTHONPATH#|$PYTHON|; s|#FILEPATH#|$MAIN|; s|#CONFPATH#|$CONF|; s|#LOGPATH#|$LOG|" $START # enters the file path into the start up file
+	sudo sed -i.backup "s|#STARTPATH#|$START|" $DIR/bin/space_machines.service # enters the start up file path into the service file
 	sudo cp $DIR/bin/space_machines.service /lib/systemd/system/space_machines.service #makes script run on boot
 	sudo chmod 644 /lib/systemd/system/space_machines.service
 	sudo systemctl daemon-reload
