5bfbc4459fe95b46f5ffdc55f32066bfcbccd053
bkarstens <barrettkarstens@gmail.com>
2016-07-21 14:48:27 -0500

    removed redundancy
    if sudo is in the get-and-run.sh, than the file needs to be run with
    sudo. running ‘sudo get-and-run.sh’ should be all thats needed.

--- bin/get-and-run.sh
+++ bin/get-and-run.sh
@@ -3,9 +3,9 @@ cd ..
 if [ -e '.git' ]
 	then git pull
 else
-	sudo rm -rf $(pwd)
+	rm -rf $(pwd)
 	cd ..
-	sudo git clone https://github.com/bvesperman/Sector67RaspberryPiAccessControl.git
+	git clone https://github.com/bvesperman/Sector67RaspberryPiAccessControl.git
 	cd Sector67RaspberryPiAccessControl
 fi
 git checkout pseudo-master
@@ -16,27 +16,27 @@ PYTHON=$(which python)
 os='Linux raspberrypi'
 MAIN=$DIR/space_machines/main.py
 kernalinfo=$(uname -a)
-sudo chmod u+x $DIR/bin/$STARTNAME
+chmod u+x $DIR/bin/$STARTNAME
 if [[ "$kernalinfo" =~ "$os" ]]; then
 	echo "OS is $os, performing additional setup."
 	isRPi=true
 	CONFNAME=rpi-machine.conf
-	sudo cp $DIR/space_machines/*.conf /etc #copy config files to /etc
+	cp $DIR/space_machines/*.conf /etc #copy config files to /etc
 	CONF=/etc/$CONFNAME
 	LOG=/etc/$LOGNAME
-	sudo cp $DIR/bin/$STARTNAME /usr/local/bin
+	cp $DIR/bin/$STARTNAME /usr/local/bin
 	START=/usr/local/bin/$STARTNAME
-	sudo apt-get install python-dev python-pip python-tk python-serial #vlc
-	sudo sed -i.backup 's|.*BLANK_TIME=.*|#BLANK_TIME=0|; s|.*BLANK_DPMS=.*|#BLANK_DPMS=off|; s|.*POWERDOWN_TIME=.*|#POWERDOWN_TIME=0|' /etc/kbd/config #disable screensaver/ screen blanking
-	sudo sed -i.backup "s|#PYTHONPATH#|$PYTHON|; s|#FILEPATH#|$MAIN|; s|#CONFPATH#|$CONF|; s|#LOGPATH#|$LOG|" $START # enters the file path into the start up file
-	sudo sed -i.backup "s|#STARTPATH#|$START|" $DIR/bin/space_machines.service # enters the start up file path into the service file
-	sudo cp $DIR/bin/space_machines.service /lib/systemd/system/space_machines.service #makes script run on boot
-	sudo chmod 644 /lib/systemd/system/space_machines.service
-	sudo systemctl daemon-reload
-	sudo systemctl enable space_machines.service
-	sudo systemctl stop serial-getty@ttyAMA0.service # terminates, disables, and keeps disabled serial TTY
-	sudo systemctl disable serial-getty@ttyAMA0.service
-	sudo systemctl mask serial-getty@ttyAMA0.service
+	apt-get install python-dev python-pip python-tk python-serial #vlc
+	sed -i.backup 's|.*BLANK_TIME=.*|#BLANK_TIME=0|; s|.*BLANK_DPMS=.*|#BLANK_DPMS=off|; s|.*POWERDOWN_TIME=.*|#POWERDOWN_TIME=0|' /etc/kbd/config #disable screensaver/ screen blanking
+	sed -i.backup "s|#PYTHONPATH#|$PYTHON|; s|#FILEPATH#|$MAIN|; s|#CONFPATH#|$CONF|; s|#LOGPATH#|$LOG|" $START # enters the file path into the start up file
+	sed -i.backup "s|#STARTPATH#|$START|" $DIR/bin/space_machines.service # enters the start up file path into the service file
+	cp $DIR/bin/space_machines.service /lib/systemd/system/space_machines.service #makes script run on boot
+	chmod 644 /lib/systemd/system/space_machines.service
+	systemctl daemon-reload
+	systemctl enable space_machines.service
+	systemctl stop serial-getty@ttyAMA0.service # terminates, disables, and keeps disabled serial TTY
+	systemctl disable serial-getty@ttyAMA0.service
+	systemctl mask serial-getty@ttyAMA0.service
 else
 	echo "OS is not $os, no additional setup performed."
 	isRPi=false
@@ -46,11 +46,11 @@ else
 	CONF=$DIR/space_machines/$CONFNAME
 fi
 cd $DIR
-sudo $PYTHON -m pip uninstall $DIR
-sudo $PYTHON -m pip install $DIR
+$PYTHON -m pip uninstall $DIR
+$PYTHON -m pip install $DIR
 
 if [[ "$kernalinfo" =~ "$os" ]]; then
-	sudo systemctl start space_machines.service
+	systemctl start space_machines.service
 else
-	sudo $START
+	$START
 fi
